fff76b6d3f3444712b9a13a2872693a9e1044c35
dlgywn2263 <lhj-3988@naver.com>
2026-05-11 20:08:17 +0900

    일정관리 , 캘린더 파일

--- app/schedules/calendar/page.tsx
+++
@@ -1,5 +0,0 @@
-import ScheduleCalendarManageMock from "@/components/schedules/ScheduleCalendarManageMock";
-
-export default function ScheduleCalendarPage() {
-  return <ScheduleCalendarManageMock />;
-}
